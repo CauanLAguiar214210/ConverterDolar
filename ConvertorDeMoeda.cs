7538d67e64a2775832227d91070a1c3349000a9f
CauanLAguiar <132419025+CauanLAguiar214210@users.noreply.github.com>
2024-06-13 22:27:43 -0300

    Commit1

---
+++ ConvertorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolar
+{
+    internal class ConvertorDeMoeda
+    {
+        public static double Contacao;
+        public static double Quantia;
+        public static double Iof = 6.0;
+
+
+        public static double DolarParaReal(double Contacao, double Quantia)
+        {
+            double Total = Contacao * Quantia;
+            return Total + Total * Iof / 100.00;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConvertorDeMoeda.Contacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConvertorDeMoeda.Quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Valor = ConvertorDeMoeda.DolarParaReal(ConvertorDeMoeda.Contacao, ConvertorDeMoeda.Quantia);
+
+            Console.WriteLine("Valor a ser pago em reais = " + Valor.ToString("F2",CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
